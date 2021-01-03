27366c653202c87479ffd0767d8aee4fe385a855
Josh <joshrazonn@gmail.com>
2021-01-03 13:08:28 -0700

    added nav scroll functionality

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+// Hamburger toggle
 const bars = document.getElementById("bars");
 function hamburgerToggle() {
   const navLinks = document.getElementById("nav-links");
@@ -11,3 +12,12 @@ function hamburgerToggle() {
 
 }
 bars.addEventListener("click", hamburgerToggle);
+
+// Nav Scroll
+const about = document.getElementById("about-link");
+const tours = document.getElementById("tours-link");
+const articles = document.getElementById("articles-link");
+
+about.onclick = () => gsap.to(window, { duration: 1.5, ease: "power4", scrollTo: { y: "#about-us", offsetY: 90 } })
+tours.onclick = () => gsap.to(window, { duration: 1.5, ease: "power4", scrollTo: { y: "#group-tours", offsetY: 90 } })
+articles.onclick = () => gsap.to(window, { duration: 1.5, ease: "power4", scrollTo: { y: "#articles", offsetY: 90 } })
